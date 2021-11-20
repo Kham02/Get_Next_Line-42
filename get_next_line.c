bda9f9ba195cc48dddc956aecf424c60abd7d520
Elvera Strong <estrong@et-i4.kzn.21-school.ru>
2021-11-20 22:43:29 +0300

    farmland

--- get_next_line.c
+++ get_next_line.c
@@ -1,27 +1,37 @@
 #include "get_next_line.h"
 
-ft_end(int len, char *buf, char *s)
+
+char	*ft_get_line(char **line, size_t len)
 {
+	char	*str;
+	char	*s2;
+	size_t	n;
 
-}
-
-char ft_read(int fd, char *buf, char *s)
-{
-	int	len;
-
-	len = 1;
-	while (!ft_strchr(buf, '\n'))
+	if (len < 0)
+		return (0);
+	if (!len && (!line || !*line))
+		return (0);
+	n = 0;
+	str = NULL;
+	while ((*line)[n] != '\n' && (*line)[n])
+		n++;
+	if ((*line)[n] == '\n')
 	{
-		if (buf[len] == '\0' && buf[len + 1] == '\n')
+		str = ft_substr(*line, 0, n + 1);
+		s2 = ft_strdup(*line + n + +1);
+		free(*line);
+		*line = s2;
+		if (!**line)
 		{
-			s = read(fd, buf, BUFFER_SIZE);
-			len++;
+			free(*line);
+			*line = NULL;
 		}
-		else if (buf[len] == '\0' && buf[len + 1] != '\n')
-			s = ft_end(len, buf, s);
+		return (str);
 	}
-	len += 1;
-	return(s);
+	str = ft_strdup(*line);
+	free(*line);
+	*line = NULL;
+	return (str);
 }
 
 char	*get_next_line(int fd)
@@ -29,20 +39,42 @@ char	*get_next_line(int fd)
 	static char	*line;
 	char		*buf;
 	char		*s;
+	size_t		len;
 
-	if (fd < 0)
-		return(0);
-	buf = (char *)malloc(sizeof(s));
+	len = 0;
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (0);
+	buf = (char *)malloc(BUFFER_SIZE + 1);
 	if (!buf)
 		return(0);
-	s = ft_read(fd, buf, s);
-	if (!s || s[0] == '\0')
+	len = read(fd, buf, BUFFER_SIZE);
+	while (len > 0)
 	{
-		free(s);
-		s = 0;
-		return(0);
+		buf[len] = '\0';
+		if (!line)
+			line = ft_strdup("");
+		s = ft_strjoin(line, buf);
+		free(line);
+		line = s;
+		if (ft_strchr(buf, '\n'))
+			break;
+		len = read(fd, buf, BUFFER_SIZE);
 	}
+	free(buf);
+	return(ft_get_line(&line, len));
+}
 
-	
-	
+int	main()
+{
+	char	*line;
+	int		fd;
+
+	fd = open("test_text.txt", O_RDONLY);
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
+	return(0);
 }
